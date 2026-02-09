5d80da7a3581100cef346c9ef1f098af95d8e7b7
Rudolf-ux <rudiktsibert002@gmail.com>
2026-02-09 13:26:33 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,24 @@
+let btnTextList = ["C", "+/-", "%", "/", 
+                    "7", "8", "9", "*",
+                    "4", "5", "6", "-",
+                    "1", "2", "3", "+",
+                   "<", "0", ".", "="];
+
+let main = document.querySelector("main");
+let previousNumber = 0;
+let operation ="";
+
+
+function createUI(){
+    let textField = document.createElement("div");
+    textField.className = "line";
+    textField.className = "textField";
+    textField.textContent = "0";
+    main.appendChild(textField);
+
+    function createUI(){}
+
+
+
+
+}
